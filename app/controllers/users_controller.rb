484cb6651fad8c2a768bf6da9ea794ad1ee4cf79
Alain Dutra <alain@drsolution.org>
2014-06-20 13:22:17 -0300

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   def new
   	@user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -19,9 +26,24 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    sign_out
-    redirect_to root_url
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      # Handle successful update
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
 
 
   private
@@ -29,5 +51,23 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,10 +9,18 @@ def full_title(page_title)
   end
 end
 
-def valid_signin(user)
-	fill_in "Email",    with: user.email
-	fill_in "Password", with: user.password
-	click_button "Sign in"
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
 end
 
 def valid_signup
@@ -21,6 +29,15 @@ def valid_signup
   fill_in "Password",     with: "foobar"
   fill_in "Confirmation", with: "foobar"
 end
+
+def valid_update(user, new_name, new_email)
+  fill_in "Name",             with: new_name
+  fill_in "Email",            with: new_email
+  fill_in "Password",         with: user.password
+  fill_in "Confirmation", with: user.password
+  click_button "Save changes"
+end
+
 
 def should_have_title_and_content(title, content)
   should have_content(content)
